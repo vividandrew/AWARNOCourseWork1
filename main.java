971ddcfc1c902429d2ed0ecb8b96d0ec187ab19b
Andrew Warnock <andrew.warnock@outlook.com>
2024-11-06 21:24:23 +0000

    Error handling

--- main.java
+++ main.java
@@ -4,24 +4,33 @@ import java.util.Scanner;
 class Dec2Hex
 {
 	public static int Arg1;
-	    public static void main(String args[])    {
-		            Arg1 = Integer.parseInt(args[0]);
-			            char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-				            int rem, num;
-					            num = Arg1;
-						            String hexadecimal="";
-							            System.out.println("Converting the Decimal Value " + num + " to Hex...");
+		public static void main(String args[])    
+		{
+			// If parsed arguments are 0 print instructions to use the command and then return
+           		if(args.length != 1 ){System.out.println("This command requires 1 argument passed to execute");return;};
 
-								            while(num != 0)
-										            {
-												                rem=num%16;
-														            hexadecimal= ch[rem] + hexadecimal;
-															                num= num/16;
-																	        }
+			try{
+				Arg1 = Integer.parseInt(args[0]);
+			}catch (NumberFormatException e)
+			{
+				System.out.println(e + " is not a valid decimal");
+				return;
+			}
+			Arg1 = Integer.parseInt(args[0]);
+			char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+			int rem, num;
+			num = Arg1;
+			String hexadecimal="";
+			System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-									            System.out.println("Hexadecimal representation is: " + hexadecimal);
-
-										        }
+			while(num != 0)
+			{
+				rem=num%16;
+				hexadecimal= ch[rem] + hexadecimal;
+				num= num/16;
+			}
+			System.out.println("Hexadecimal representation is: " + hexadecimal);
+		}
 }
 
 
